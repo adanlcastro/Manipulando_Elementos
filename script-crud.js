100252a69f2980ba18ff6d9562fb25e0ea9ff641
adanlcastro <adan.castro.contato@gmail.com>
2025-04-05 16:40:37 -0300

    adicionar tarefaz, editar e tarefa finalizada
    atualização do código para novas funções

--- script-crud.js
+++ script-crud.js
@@ -1,9 +1,19 @@
 const btnAdicionarTarefa = document.querySelector('.app__button--add-task')
 const formAdicionarTarefa = document.querySelector ('.app__form-add-task')
 const textArea = document.querySelector ('.app__form-textarea')
-const ulTarefas = documento.querySelector ('.app__section-task-list')
+const ulTarefas = document.querySelector ('.app__section-task-list')
+const cancelarBtn = document.querySelector ('.app__form-footer__button--cancel')
+const deletarBtn = document.querySelector ('.app__form-footer__button--delete')
+
+const paragrafoDescricaoTarefa = document.querySelector('.app__section-active-task-description')
 
 const tarefas = JSON.parse(localStorage.getItem('tarefas')) || []
+let tarefaSelecionada = null ;
+let liTarefaSelecionada = null ;
+
+function atualizarTarefas () {
+    localStorage.setItem('tarefas', JSON.stringify(tarefas))
+}
 
 function criarElementoTarefa (tarefa){
     const li = document.createElement('li')
@@ -11,24 +21,63 @@ function criarElementoTarefa (tarefa){
 
     const svg = document.createElement ('svg');
     svg.innerHTML = `
-    <svg>
         <svg class="app__section-task-icon-status" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
             <circle cx="12" cy="12" r="12" fill="#FFF"></circle>
             <path d="M9 16.1719L19.5938 5.57812L21 6.98438L9 18.9844L3.42188 13.4062L4.82812 12L9 16.1719Z" fill="#01080E"></path>
         </svg>
-    </svg>
     `
 
     const paragrafo = document.createElement('p');
+    paragrafo.classList.add ('app__section-task-list-item-description')
     paragrafo.textContent = tarefa.descricao
 
     const botao = document.createElement ('button');
+    botao.classList.add ('app_button-edit')
+
+    //editar item da lista
+    botao.onclick = () => {
+
+        const novaDescricao = prompt ('Qual e o novo nome da tarefa?');
+
+        if (novaDescricao){
+
+        paragrafo.textContent = novaDescricao ;
+        tarefa.descricao = novaDescricao ;
+
+        atualizarTarefas()
+        } if (novaDescricao == '') {
+            alert ('o campo esta vazio');
+            return
+        }
+
+    }
+
     const imagemBtn = document.createElement ('img');
     imagemBtn.setAttribute ('src', "/imagens/edit.png")
 
     botao.append(imagemBtn)
-    li.append(paragrafo)
-    li.append(botao)
+    li.append(svg, paragrafo, botao)
+
+    li.onclick = () => {
+        document.querySelectorAll ('.app__section-task-list-item-active')
+            .forEach(elemento => {
+                elemento.classList.remove('app__section-task-list-item-active')
+            })
+
+        if (tarefaSelecionada == tarefa){
+            paragrafoDescricaoTarefa.textContent = '' ;
+            elemento.classList
+            tarefaSelecionada = null
+            liTarefaSelecionada = null
+            return
+        }
+
+        tarefaSelecionada = tarefa
+        liTarefaSelecionada = li
+        paragrafoDescricaoTarefa.textContent = tarefa.descricao
+        
+        li.classList.add ('app__section-task-list-item-active')
+    }
 
     return li
 }
@@ -44,10 +93,34 @@ formAdicionarTarefa.addEventListener ('submit', (evento) => {
     }
 
     tarefas.push (tarefa)
-    localStorage.setItem('tarefas', JSON.stringify(tarefas))
-})
+   
+    
+    const elementoTarefa = criarElementoTarefa(tarefa);
+    ulTarefas.append(elementoTarefa)
+
+    atualizarTarefas()
+
+    textArea.value = '';
+    formAdicionarTarefa.classList.add('hidden');
+});
 
 tarefas.forEach (tarefa => {
     const elementoTarefa = criarElementoTarefa(tarefa)
     ulTarefas.append(elementoTarefa)
-})
+});
+
+cancelarBtn.addEventListener ('click', () => {
+    formAdicionarTarefa.classList.add ('hidden');
+});
+
+deletarBtn.onclick = () => {
+    textArea.value = ''
+};
+
+document.addEventListener ('FocoFinalizado', () =>{
+    if(tarefaSelecionada && liTarefaSelecionada) {
+        liTarefaSelecionada.classList.remove ('app__section-task-list-item-active')
+        liTarefaSelecionada.classList.add ('app__section-task-list-item-complete')
+        liTarefaSelecionada.querySelector('button').setAttribute('disabled', 'disabled')
+    }
+} )
--- script.js
+++ script.js
@@ -1,92 +1,76 @@
-const html = document.querySelector ('html');
-const displayTempo = document.querySelector('#timer');
-const tempoNatela = document.querySelector ('#timer')
+const html = document.querySelector('html');
+const tempoNaTela = document.querySelector('#timer');
+const startPauseBtn = document.getElementById('start-pause');
+const playouPause = document.querySelector('#start-pause span');
+const playouPauseImg = document.querySelector('.app__card-primary-butto-icon');
+const focoBt = document.querySelector('.app__card-button--foco');
+const curtoBt = document.querySelector('.app__card-button--curto');
+const longoBt = document.querySelector('.app__card-button--longo');
+const botoes = document.querySelectorAll('.app__card-button');
+const titulo = document.querySelector('.app__title');
+const banner = document.querySelector('.app__image');
+const musicaFocoInput = document.querySelector('#alternar-musica');
 
-const startPauseBtn = document.getElementById ('start-pause')
-const playouPause = document.querySelector ('#start-pause span')
-const playouPauseImg = document.querySelector('.app__card-primary-butto-icon')
-const focoBt = document.querySelector ('.app__card-button--foco')
-const curtoBt = document.querySelector ('.app__card-button--curto')
-const longoBt = document.querySelector ('.app__card-button--longo')
-const botoes = document.querySelectorAll ('.app__card-button')
-
-const titulo = document.querySelector('.app__title')
-const tituloDestacado = document.querySelector ('.app__title-strong')
-
-const banner = document.querySelector('.app__image')
-
-const musicaFocoInput = document.querySelector ('#alternar-musica')
-
-const musica = new Audio('/sons/luna-rise-part-one.mp3')
+const musica = new Audio('/sons/luna-rise-part-one.mp3');
 musica.loop = true;
 
 musicaFocoInput.addEventListener('change', () => {
-    if (musica.paused){
-        musica.play()
+    if (musica.paused) {
+        musica.play();
     } else {
-        musica.pause()
+        musica.pause();
     }
-})
+});
 
-let tempoDecorridoEmSegundos = 1500
-let intervaloID = null
+let tempoDecorridoEmSegundos = 1500;
+let intervaloID = null;
 
-const duracaoFoco = 1500
-const duracaoCurto =  900
-const duracaoLongo = 300
-
+const duracaoFoco = 1500;
+const duracaoCurto = 900;
+const duracaoLongo = 1800;
 
 focoBt.addEventListener('click', () => {
-    tempoDecorridoEmSegundos = 1500
-    alterarContexto('foco')
-    focoBt.classList.add ('active')
-})
+    tempoDecorridoEmSegundos = duracaoFoco;
+    alterarContexto('foco');
+    focoBt.classList.add('active');
+});
 
 curtoBt.addEventListener('click', () => {
-    tempoDecorridoEmSegundos = 300
-    alterarContexto('descanso-curto')
-    curtoBt.classList.add ('active')
-})
+    tempoDecorridoEmSegundos = duracaoCurto;
+    alterarContexto('descanso-curto');
+    curtoBt.classList.add('active');
+});
 
 longoBt.addEventListener('click', () => {
-    tempoDecorridoEmSegundos = 900
-    alterarContexto('descanso-longo')
-    longoBt.classList.add ('active')
-})
+    tempoDecorridoEmSegundos = duracaoLongo;
+    alterarContexto('descanso-longo');
+    longoBt.classList.add('active');
+});
 
-function alterarContexto (contexto){
-    mostrarTempo()
-    botoes.forEach(function (contexto){
-        contexto.classList.remove('active');
-    })
-    html.setAttribute ('data-contexto', contexto)
-    banner.setAttribute ('src', `/imagens/${contexto}.png`)
+function alterarContexto(contexto) {
+    mostrarTempo();
+    botoes.forEach((botao) => botao.classList.remove('active'));
+    html.setAttribute('data-contexto', contexto);
+    banner.setAttribute('src', `/imagens/${contexto}.png`);
 
-    switch(contexto){
-        case "foco":
+    switch (contexto) {
+        case 'foco':
             titulo.innerHTML = `
-            Otimize sua produtividade,<br>
-                <strong class="app__title-strong">mergulhe no que importa.</strong>
-            `
-
-        break;
-        case "descanso-curto":
-        titulo.innerHTML = `
-            Que tal uma respirada?<br>
-                <strong class="app__title-strong">faça uma pausa</strong>
-            `
-        
-        break
-        case "descanso-longo":
-        titulo.innerHTML = `
-            Hora de voltar a superfice<br>
-                <strong class="app__title-strong">faça uma pausa longa</strong>
-            `
-
-        break
-        
+                Otimize sua produtividade,<br>
+                <strong class="app__title-strong">mergulhe no que importa.</strong>`;
+            break;
+        case 'descanso-curto':
+            titulo.innerHTML = `
+                Que tal uma respirada?<br>
+                <strong class="app__title-strong">faça uma pausa</strong>`;
+            break;
+        case 'descanso-longo':
+            titulo.innerHTML = `
+                Hora de voltar à superfície<br>
+                <strong class="app__title-strong">faça uma pausa longa</strong>`;
+            break;
         default:
-        break
+            break;
     }
 }
 
@@ -94,47 +78,50 @@ const somPause = new Audio('/sons/pause.mp3');
 const somPlay = new Audio('/sons/play.wav');
 const somBeep = new Audio('/sons/beep.mp3');
 
-
-const contagemRegressiva = () =>{
+const contagemRegressiva = () => {
     if (tempoDecorridoEmSegundos <= 0) {
-        zerar()
-        somBeep.play()
-        alert ('tempo finalizado')
-        return
+        somBeep.play();
+        alert('Tempo finalizado');
+        const focoAtivo = html.getAttribute('data-contexto') == 'foco';
+        if (focoAtivo) {
+            const evento = new CustomEvent('FocoFinalizado');
+            document.dispatchEvent(evento);
+        }
+        zerar();
+        return;
     }
-    tempoDecorridoEmSegundos -= 1
-    mostrarTempo()
-}
+    tempoDecorridoEmSegundos -= 1;
+    mostrarTempo();
+};
 
-startPauseBtn.addEventListener ('click', iniciarOuPausar)
+startPauseBtn.addEventListener('click', iniciarOuPausar);
 
-function iniciarOuPausar(){
-    if (intervaloID){
-        zerar()
-        somPause.play()
-        return
+function iniciarOuPausar() {
+    if (intervaloID) {
+        zerar();
+        somPause.play();
+        return;
     }
-        somPlay.play()
-        intervaloID = setInterval(contagemRegressiva, 1000)
-        playouPauseImg.src = src="/imagens/pause.png"
-        playouPause.textContent = 'Pausar'
-
+    somPlay.play();
+    intervaloID = setInterval(contagemRegressiva, 1000);
+    playouPauseImg.src = "/imagens/pause.png";
+    playouPause.textContent = 'Pausar';
 }
 
-function zerar () {
-    clearInterval(intervaloID)
-    playouPause.textContent = 'Começar'
-    playouPauseImg.src = src="/imagens/play_arrow.png"
-    intervaloID = null
+function zerar() {
+    clearInterval(intervaloID);
+    intervaloID = null;
+    playouPause.textContent = 'Começar';
+    playouPauseImg.src = "/imagens/play_arrow.png";
 }
 
-function mostrarTempo () {
-    const tempo = new Date (tempoDecorridoEmSegundos * 1000);
-    const tempoFormatado = tempo.toLocaleTimeString ('pt-br', {
+function mostrarTempo() {
+    const tempo = new Date(tempoDecorridoEmSegundos * 1000);
+    const tempoFormatado = tempo.toLocaleTimeString('pt-br', {
         minute: '2-digit',
         second: '2-digit'
-    })
-    tempoNatela.innerHTML = `${tempoFormatado}`
+    });
+    tempoNaTela.innerHTML = `${tempoFormatado}`;
 }
 
-mostrarTempo()
+mostrarTempo();
